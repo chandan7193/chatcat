31f49c2cb561369ebf37a161238326da7a761a4e
Chandan Singh <chandan.singh7193@gmail.com>
2018-03-20 20:17:02 +0530

    Strange way of writing routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -11,9 +11,28 @@ module.exports=()=>{
                 res.render('rooms');
             },
             '/chat':(req,res,next)=>{
-                res.render('chat');
-            }
+                res.render('chatroom');
+            },
+        },
+        'post':{
 
+        }       
+
+    }
+    let registerRoutes = (routes, method)=>{
+        for(let key in routes ){
+            if(typeof routes[key]==='object' && routes[key] !==null && !(routes[key] instanceof Array)){
+                registerRoutes(routes[key],key);
+            }else{
+                if(method==='get'){
+                    router.get(key, routes[key]);
+                }else if(method==='post'){
+                    router.post(key, routes[key]);
+                }
+            }
+                
         }
     }
+    registerRoutes(routes);
+    return router;  
 }
